ce405b14d8734b985be925f7d8f612bcfeabf01a
ConspiracyTheorist 27 <matthew_occleshaw@outlook.com>
2021-01-13 12:45:40 +0000

    Moved playerClass to player.py and changed all imports from wildcard to specific

--- enemy.py
+++ enemy.py
@@ -1,26 +1,30 @@
 from random import randint
 
 class enemyParentClass:
+  def __init__(self, current_room):
+    self.current_room = current_room
+
   def attackEnemy(self, target):
     self.damage = self.attack + randint(0, self.attack)
     target.health -= self.damage
 
 class basicEnemyClass(enemyParentClass):
-  def __init__(self ):
+  def __init__(self, current_room):
+    enemyParentClass.__init__(current_room)
     self.type = "Basic Enemy"
     self.health = 15
     self.speed = 35
     self.attack = 5
 
 class normalEnemyClass(enemyParentClass):
-  def __init__(self ):
+  def __init__(self):
     self.type = "Normal Enemy"
     self.health = 25
     self.speed = 50
     self.attack = 10
 
 class hardEnemyClass(enemyParentClass):
-  def __init__(self ):
+  def __init__(self):
     self.type = "Hard Enemy"
     self.health = 35
     self.speed = 65
--- location.py
+++ location.py
@@ -13,3 +13,13 @@ class locationClass: #defines class for rooms
     for i in self.connected_rooms:
       connected_rooms_names.append(i.room_name)
     return connected_rooms_names
+
+l9 = locationClass("9", 0, 0, 0, 1, [])
+l8 = locationClass("8", 1, 2, 1, 0, [l9])
+l7 = locationClass("7", 1, 0, 1, 0, [l8])
+l6 = locationClass("6", 1, 0, 1, 0, [l8])
+l5 = locationClass("5", 1, 0, 1, 0, [l8])
+l4 = locationClass("4", 1, 1, 0, 0, [l6, l7])
+l3 = locationClass("3", 1, 1, 0, 0, [l5])
+l2 = locationClass("2", 3, 0, 0, 0, [l3, l4], "apple", "upgrade_station")
+l1 = locationClass("1", 1, 0, 0, 0, [l2], "key")
