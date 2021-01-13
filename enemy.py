bebb5ae6411e5b5e9c8d4ef735f113fb59095927
ConspiracyTheorist 27 <matthew_occleshaw@outlook.com>
2021-01-13 12:12:24 +0000

    Enemy defining code split into enemy.py

---
+++ enemy.py
@@ -0,0 +1,34 @@
+from random import randint
+
+class enemyParentClass:
+  def attackEnemy(self, target):
+    self.damage = self.attack + randint(0, self.attack)
+    target.health -= self.damage
+
+class basicEnemyClass(enemyParentClass):
+  def __init__(self ):
+    self.type = "Basic Enemy"
+    self.health = 15
+    self.speed = 35
+    self.attack = 5
+
+class normalEnemyClass(enemyParentClass):
+  def __init__(self ):
+    self.type = "Normal Enemy"
+    self.health = 25
+    self.speed = 50
+    self.attack = 10
+
+class hardEnemyClass(enemyParentClass):
+  def __init__(self ):
+    self.type = "Hard Enemy"
+    self.health = 35
+    self.speed = 65
+    self.attack = 15
+
+class bossEnemyClass(enemyParentClass):
+  def __init__(self):
+    self.type = "Boss Enemy"
+    self.health = 75
+    self.speed = 100
+    self.attack = 25
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from random import randint #imports random module
+from random import randint #imports randint from random module
+from enemy import *
+
 class playerClass: #defines player class
   def __init__(self):
     self.health = 100
@@ -6,12 +8,18 @@ class playerClass: #defines player class
     self.speed = 50
     self.attack = 10
     self.backpack_size = 3
-    self.backpack = [] #backpack array contains carried items
-    self.current_room = "l1"
+    self.backpack = [] #backpack list contains carried items
+    self.current_room = l1
   
   def pickUpItem(self, item): #defines the method to pick up an item - takes the item's name
     if len(self.backpack) >= self.backpack_size:
-      self.swapItem = input("Your backpack is full. Would you like to swap")
+      self.swap_item = input("Your backpack is full. Would you like to swap this item with an item you currently have?(y/n): ")
+      if self.swap_item == "y":
+        print("Items currently in bag:")
+        for i in range(self.backpack_size):
+          print(str(i+1) + ": " + self.backpack[i])
+        self.swap_item = int(input("Enter the number of the item you would like to swap: "))
+        self.backpack[self.swap_item - 1] = item
     else:
       self.backpack.append(item)
 
@@ -23,6 +31,7 @@ class playerClass: #defines player class
   
   def encounterEnemy(self, target):
     print("You have encountered a", target.type)
+    #***PLACEHOLDER***
   
   def useUpgradeStation(self):
     while True:
@@ -49,46 +58,37 @@ class playerClass: #defines player class
       else:
         print("That is not a valid thing to upgrade. Please try again.")
 
-
-
-class enemyParentClass:
-  def attackEnemy(self, target):
-    self.damage = self.attack + randint(0, self.attack)
-    target.health -= self.damage
-
-class basicEnemyClass(enemyParentClass):
-  def __init__(self ):
-    self.type = "Basic Enemy"
-    self.health = 15
-    self.speed = 35
-    self.attack = 5
-
-class normalEnemyClass(enemyParentClass):
-  def __init__(self ):
-    self.type = "Normal Enemy"
-    self.health = 25
-    self.speed = 50
-    self.attack = 10
-
-class hardEnemyClass(enemyParentClass):
-  def __init__(self ):
-    self.type = "Hard Enemy"
-    self.health = 35
-    self.speed = 65
-    self.attack = 15
-
-
+  def enterRoom(self):
+    pass #***PLACEHOLDER***
+  
+  def changeRoom(self):
+    print(self.current_room.connected_rooms)
 
 class locationClass: #defines class for rooms
-  def __init__(self, basic_enemy, normal_enemy, hard_enemy, *items):
+  def __init__(self, room_name, basic_enemy, normal_enemy, hard_enemy, boss, connected_rooms, *items):
+    self.room_name = room_name
     self.basic_enemy = basic_enemy
     self.normal_enemy = normal_enemy
     self.hard_enemy = hard_enemy
-    self.items = items #could be an array?
+    self.boss = boss
+    self.connected_rooms = connected_rooms
+    self.items = list(items) #returns a list
 
+  def return_connected_rooms(self):
+    connected_rooms_names = []
+    for i in self.connected_rooms:
+      connected_rooms_names.append(i.room_name)
+    return connected_rooms_names
+    
+l9 = locationClass("9", 0, 0, 0, 1, [])
+l8 = locationClass("8", 1, 2, 1, 0, [l9])
+l7 = locationClass("7", 1, 0, 1, 0, [l8])
+l6 = locationClass("6", 1, 0, 1, 0, [l8])
+l5 = locationClass("5", 1, 0, 1, 0, [l8])
+l4 = locationClass("4", 1, 1, 0, 0, [l6, l7])
+l3 = locationClass("3", 1, 1, 0, 0, [l5])
+l2 = locationClass("2", 3, 0, 0, 0, [l3, l4], "apple", "upgrade_station")
+l1 = locationClass("1", 1, 0, 0, 0, [l2], "key")
 
-
-p1 = playerClass() #creates the player object
-l1 = locationClass(1, 0, 0) #creates the first room (referenced in player class so deletion would cause errors)
-
-p1.useUpgradeStation()
+p1 = playerClass()
+print(l4.return_connected_rooms())
