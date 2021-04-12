1b8c1ce486915a32a01f34a18384861b98811dad
matthew-occleshaw <61629073+matthew-occleshaw@users.noreply.github.com>
2021-04-12 21:00:15 +0100

    Added introduction to main.py

--- main.py
+++ main.py
@@ -3,7 +3,17 @@ from enemy import BasicEnemyClass, NormalEnemyClass, HardEnemyClass, BossEnemyCl
 # noinspection PyUnresolvedReferences
 from location import LocationClass, l1, l2, l3, l4, l5, l6, l7, l8, l9
 from player import PlayerClass
+from item import Apple, Key, UpgradeStation
 
-p1 = PlayerClass(l1)
-print(p1.current_room.return_connected_rooms())
-e1 = BasicEnemyClass(l1)
+
+p0 = PlayerClass(l1)
+
+
+input("Press ENTER to start")
+username = input("Enter username: ")
+print("\nYou awake in a small, dingy room",
+"The walls are made of stone brick, and there are no windows",
+"However, there is one doorway on the other side of the room, which you walk through",
+sep="\n", end="")
+
+p0.fight()
