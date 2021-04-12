da148f6fafd7e4f7c0425624cfa42eae06de44b7
matthew-occleshaw <61629073+matthew-occleshaw@users.noreply.github.com>
2021-04-12 20:56:04 +0100

    Create item.py

---
+++ item.py
@@ -0,0 +1,11 @@
+class Apple:
+    def __init__(self):
+        self.health_increase = 15
+
+class Key:
+    def __init__(self, room_for):
+        self.room = room_for
+
+class UpgradeStation:
+    def __init__(self):
+        self.used = False
