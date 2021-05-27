95f5ca7ce2c8dbcfcfa55afc0df643f7e5dacb00
matthew-occleshaw <61629073+matthew-occleshaw@users.noreply.github.com>
2021-05-27 19:20:31 +0100

    Finish implementing store_game_data

--- save_game.py
+++ save_game.py
@@ -1,4 +1,9 @@
 from json import loads, dumps
+from datetime import datetime
+from sys import argv
+
+from item import items_dict
+from location import locations_dict
 
 
 def store_game_state(player):
@@ -12,10 +17,14 @@ def store_game_state(player):
         file_data = []
 
     game_data = vars(player)
+    game_data["current_room"] = player.current_room.room_name
+    game_data["backpack"] = [item.name for item in player.backpack]
+    current_time = datetime.now()
+    time_string = current_time.strftime("%H:%M:%S, %d/%m/%y")
+    game_data["save_name"] = f"{player.username} @ {time_string}"
     file_data.append(game_data)
-    with open("game_saves.json", "wt"):
-        pass
-    # TODO Implement game state storage
+    with open("game_saves.json", "wt") as write_file:
+        write_file.write(dumps(file_data))
 
 
 def retrieve_game_state():
