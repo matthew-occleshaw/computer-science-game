635fdf0ab43a60f7baca14e88f9c547591f0407e
matthew-occleshaw <61629073+matthew-occleshaw@users.noreply.github.com>
2021-05-27 18:36:41 +0100

    Add test.py to .gitignore

--- test.py
+++
@@ -1,8 +0,0 @@
-from enemy import BasicEnemyClass
-from player import PlayerClass
-from save_game import *
-
-p = PlayerClass("username")
-e = BasicEnemyClass()
-e.attack_enemy(p)
-store_game_state(p)
